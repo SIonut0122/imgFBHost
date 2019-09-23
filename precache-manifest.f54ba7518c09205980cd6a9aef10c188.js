9df04acb10aeb3813f5f3b70857c66a94a20c7f6
SIonut0122 <Sionut012@yahoo.com>
2019-09-23 14:03:25 +0100

    Updates

---
+++ precache-manifest.f54ba7518c09205980cd6a9aef10c188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e296c8c22a7404bcb12ce0aeeb724853",
+    "url": "/imgFBHost/index.html"
+  },
+  {
+    "revision": "e0a56bd111d209bc26a0",
+    "url": "/imgFBHost/static/css/main.9d678ed0.chunk.css"
+  },
+  {
+    "revision": "32db8ae2181970c676fc",
+    "url": "/imgFBHost/static/js/2.78ffe05e.chunk.js"
+  },
+  {
+    "revision": "e0a56bd111d209bc26a0",
+    "url": "/imgFBHost/static/js/main.5a56626d.chunk.js"
+  },
+  {
+    "revision": "ff1775904bb6af9ccc15",
+    "url": "/imgFBHost/static/js/runtime~main.b95e7a16.js"
+  },
+  {
+    "revision": "904cb52644d251893d650870320065d9",
+    "url": "/imgFBHost/static/media/logo_img.904cb526.jpg"
+  }
+]);
